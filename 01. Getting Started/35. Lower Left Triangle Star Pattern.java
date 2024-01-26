9c0a358e85a6d931715253d31fb94e4c0a5aa708
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2024-01-26 10:39:26 +0530

    Lower Left Triangle Star Pattern

---
+++ 01. Getting Started/35. Lower Left Triangle Star Pattern.java
@@ -0,0 +1,36 @@
+Sample Input :
+5
+  
+Sample Output :
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+
+import java.io.*;
+import java.util.*;
+public class Solution 
+{
+    public static void lowerLeftTriangleStarPattern(int n)
+    {
+        int nst = 1;
+        for(int row = 1; row <= n; row++)
+        {
+            for(int cst = 1; cst <= nst; cst++)
+            {
+                System.out.print("* ");
+            }
+            nst++;
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) 
+    {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        lowerLeftTriangleStarPattern(n);
+    }
+}
