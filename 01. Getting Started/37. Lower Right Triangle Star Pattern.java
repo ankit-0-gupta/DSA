3b3b80c78705ab8b77b05d14abafaaf159dbc533
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2024-01-26 12:44:52 +0530

    Lower Right Triangle Star Pattern

---
+++ 01. Getting Started/37. Lower Right Triangle Star Pattern.java
@@ -0,0 +1,42 @@
+Sample Input :
+5
+  
+Sample Output :
+    *
+   **
+  ***
+ ****
+*****
+
+
+import java.io.*;
+import java.util.*;
+public class Solution 
+{
+    public static void lowerRightTriangleStarPattern(int n)
+    {
+        int nsp = n - 1;
+        int nst = 1;
+        for(int row = 1; row <= n; row++)
+        {
+            for(int csp = 1; csp <= nsp; csp++)
+            {
+                System.out.print(" ");
+            }
+            for(int cst = 1; cst <= nst; cst++)
+            {
+                System.out.print("*");
+            }
+            nsp--;
+            nst++;
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) 
+    {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        lowerRightTriangleStarPattern(n);
+    }
+}
