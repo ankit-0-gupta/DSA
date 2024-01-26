3124cf903f2a9b2f1192eca34f8943ed98285d5f
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2024-01-26 13:36:40 +0530

    Right Triangle Number Pattern

---
+++ 01. Getting Started/39. Right Triangle Number Pattern.java
@@ -0,0 +1,36 @@
+// Sample Input : 
+// 5
+
+// Sample Output :
+// 1
+// 23
+// 345
+// 4567
+// 56789
+
+
+import java.util.*;
+public class Main 
+{
+    public static void rightTriangleNumberPattern(int n)
+    {
+        int nst = 1;
+        for(int row = 1; row <= n; row++)
+        {
+            int val = row;
+            for(int cst = 1; cst <= nst; cst++)
+            {
+                System.out.print(val++);
+            }
+            nst++;
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) 
+    {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        rightTriangleNumberPattern(n);
+    }
+}
