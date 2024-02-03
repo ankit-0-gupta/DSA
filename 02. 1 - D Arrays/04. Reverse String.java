3b2b663dfec3dcfad5a590d447168571fd34d1fb
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2024-02-03 10:29:29 +0530

    Reverse String

---
+++ 02. 1 - D Arrays/04. Reverse String.java
@@ -0,0 +1,26 @@
+// Reverse String.
+
+// https://leetcode.com/problems/reverse-string/
+
+
+class Solution
+{
+    public void swap(char[] s, int left, int right)
+    {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+    }
+    
+    public void reverseString(char[] s)
+    {
+        int left = 0;
+        int right = s.length - 1;
+        while(left < right)
+        {
+            swap(s, left, right);
+            left++;
+            right--;
+        }    
+    }
+}
