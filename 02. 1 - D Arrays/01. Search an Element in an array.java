e4e163901566a740afe7bfa4f51cc08b75e56d77
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2024-02-03 10:25:29 +0530

    Search an Element in an array

---
+++ 02. 1 - D Arrays/01. Search an Element in an array.java
@@ -0,0 +1,21 @@
+// Search an Element in an array.
+
+// https://www.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1
+
+
+class Solution
+{
+    static int search(int arr[], int N, int X)
+    {
+        int foundAtIndex = -1;
+        for(int i = 0; i < N; i++)
+        {
+            if(arr[i] == X)
+            {
+                foundAtIndex = i;
+                break;
+            }
+        }
+        return foundAtIndex;
+    }
+}
