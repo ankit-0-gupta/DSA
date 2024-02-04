5a31156ea41357489d313bd5a58a1a4a303190a0
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2024-02-04 10:06:15 +0530

    Number of Good Pairs

---
+++ 02. 1 - D Arrays/07. Number of Good Pairs.java
@@ -0,0 +1,23 @@
+// Number of Good Pairs.
+
+// https://leetcode.com/problems/number-of-good-pairs/
+
+
+class Solution 
+{
+    public int numIdenticalPairs(int[] nums) 
+    {
+        int goodPairs = 0;
+        for(int i = 0; i < nums.length - 1; i++)
+        {
+            for(int j = i + 1; j < nums.length; j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    goodPairs++;
+                }
+            }
+        }    
+        return goodPairs;
+    }
+}
